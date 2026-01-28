4b7b37ede98399b31abd5adf1bdc9690ec78415a
SHAMOON A <shamoon@SHAMOONs-MacBook-Pro.local>
2026-01-28 15:51:16 +0400

    Clean Code

--- terraform/envs/poc/main.tf
+++ terraform/envs/poc/main.tf
@@ -1,16 +1,16 @@
 module "gateway_vpc" {
-  source = "../../modules/vpc"
-  name   = "vpc-gateway"
-  cidr   = "10.0.0.0/16"
-  azs    = ["us-east-1a", "us-east-1b"]
+source = "../../modules/vpc"
+name = "vpc-gateway"
+cidr = var.gateway_vpc_cidr
+azs = var.availability_zones
 }
 
 
 module "backend_vpc" {
-  source = "../../modules/vpc"
-  name   = "vpc-backend"
-  cidr   = "10.1.0.0/16"
-  azs    = ["us-east-1a", "us-east-1b"]
+source = "../../modules/vpc"
+name = "vpc-backend"
+cidr = var.backend_vpc_cidr
+azs = var.availability_zones
 }
 
 
@@ -18,36 +18,33 @@ module "backend_vpc" {
 
 
 module "eks_gateway" {
-  source           = "../../modules/eks"
-  name             = "eks-gateway"
-  subnets          = module.gateway_vpc.private_subnets
-  sg_id            = module.gateway_vpc.eks_sg
-  cluster_role_arn = aws_iam_role.eks_gateway_cluster.arn
-  node_role_arn    = aws_iam_role.eks_gateway_nodes.arn
+source = "../../modules/eks"
+name = var.gateway_cluster_name
+subnets = module.gateway_vpc.private_subnets
+sg_id = module.gateway_vpc.eks_sg
+cluster_role_arn = aws_iam_role.eks_gateway_cluster.arn
+node_role_arn = aws_iam_role.eks_gateway_nodes.arn
 }
 
 
 module "eks_backend" {
-  source           = "../../modules/eks"
-  name             = "eks-backend"
-  subnets          = module.backend_vpc.private_subnets
-  sg_id            = module.backend_vpc.eks_sg
-  cluster_role_arn = aws_iam_role.eks_backend_cluster.arn
-  node_role_arn    = aws_iam_role.eks_backend_nodes.arn
+source = "../../modules/eks"
+name = var.backend_cluster_name
+subnets = module.backend_vpc.private_subnets
+sg_id = module.backend_vpc.eks_sg
+cluster_role_arn = aws_iam_role.eks_backend_cluster.arn
+node_role_arn = aws_iam_role.eks_backend_nodes.arn
 }
 
 module "vpc_peering_gateway_backend" {
   source = "../../modules/vpc-peering"
 
-  # VPC IDs
   vpc_a_id = module.gateway_vpc.vpc_id
   vpc_b_id = module.backend_vpc.vpc_id
 
-  # CIDRs
   cidr_a = module.gateway_vpc.cidr
   cidr_b = module.backend_vpc.cidr
 
-  # 🚨 PRIVATE route tables ONLY
   rt_a = module.gateway_vpc.private_route_table_id
   rt_b = module.backend_vpc.private_route_table_id
 }
